5064df57584a3d68d560a53267034efff858f45e
chau.hoang <chau.hoang@vndirect.com.vn>
2015-07-18 12:30:06 +0700

    Ramup more connections and counting

--- client.js
+++ client.js
@@ -1,13 +1,20 @@
-var CLIENT_NUMBER = 3;
+var CLIENT_NUMBER = 5000;
 var HOST = 'http://localhost:9999';
+var NEW_CONN = 100;  // number of new connection per sec
 var SockJS = require('sockjs-client');
 
 
+var new_conn_interval = 1000 / NEW_CONN;
+var count = 0;
+
 for(var i = 0; i < CLIENT_NUMBER; i++){
+  setTimeout(function(){
 var client = SockJS(HOST + '/echo');
 
 client.onopen = function(){
-	console.log('[INFO] Socket Open...');
+	count += 1;
+	console.log('[INFO] Socket Open, now total: ' + count);
+	client.send('Hello');
 };
 
 client.onclose = function(){
@@ -15,11 +22,7 @@ client.onclose = function(){
 };
 
 client.onmessage = function(e){
-	console.log('[INFO] Received' + e.data);
 }
-
-setTimeout(function(){
-	client.send('Hello');
-}, 500);
+  }, i * new_conn_interval);
 }
 
--- server.js
+++ server.js
@@ -6,14 +6,21 @@ var node_static = require('node-static');
 // 1. Echo sockjs server
 var sockjs_opts = {sockjs_url: "http://cdn.jsdelivr.net/sockjs/0.3.4/sockjs.min.js"};
 
+var conn_count = 0;
 var sockjs_echo = sockjs.createServer(sockjs_opts);
 sockjs_echo.on('connection', function(conn) {
-    console.log('[INFO] New connection at: ' + (new Date()).toString());
+    conn_count += 1;
+    console.log('[INFO] ' + (new Date()).toString() + ' New connection. Now total: ' + conn_count);
     conn.on('data', function(message) {
 	setInterval(function(){
 		conn.write(Math.random() + ":::" + (new Date()).toString() + " This is a long enough message to simulate small packets in the network");
 	}, 50);
         conn.write("[ERROR] Finished internal");
+    });
+
+    conn.on('close', function(){
+	conn_count -= 1;
+	console.log('[INFO] ' + (new Date()).toString() + ' Connection closed. Now total: ' + conn_count);
     });
 });
 
